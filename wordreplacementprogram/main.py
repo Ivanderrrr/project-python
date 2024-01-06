8b2147bebca708c8e59d7195ef2415c4cc936f69
Christoper Ivander <124675488+Ivanderrrr@users.noreply.github.com>
2024-01-06 08:49:36 +0700

    Add files via upload

---
+++ wordreplacementprogram/main.py
@@ -0,0 +1,7 @@
+def replace_word():
+        string = "Hi guys, I am Ivander, hi hi hi hi hi"
+        word_to_replace = input("enter the word to replace: ")
+        word_replacement = input("enter the word to replacement: ")
+        print(string.replace(word_to_replace, word_replacement))
+
+replace_word()
